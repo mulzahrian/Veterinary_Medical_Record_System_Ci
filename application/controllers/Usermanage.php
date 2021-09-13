1cb5a8bafe571616ab8f218138ad733a80db3058
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-09-13 13:45:43 +0700

    Update Usermanage.php
    modify data

--- application/controllers/Usermanage.php
+++ application/controllers/Usermanage.php
@@ -31,18 +31,12 @@ class Usermanage extends CI_Controller
 		$this->load->view('templates/footer');
 	}
 
-	//hapus
-
 	public function hapus($id)
     {
         $this->Usermanage_model->hapusDataUser($id);
         $this->session->set_flashdata('flash', 'Dihapus');
         redirect('usermanage');
     }
-
-	//hapus
-
-	//ubah 
 
 	public function ubah($id)
     {
@@ -51,26 +45,10 @@ class Usermanage extends CI_Controller
         $this->session->userdata('email')])->row_array();
         $data['ubah'] = $this->Usermanage_model->getUserById($id);
         
-
-       //$this->form_validation->set_rules('id_hewan', 'Hewan', 'required');
         $this->form_validation->set_rules('nama', 'nama', 'required');
         $this->form_validation->set_rules('email', 'email', 'required');
         $this->form_validation->set_rules('role_id', 'role_id', 'required');
         $this->form_validation->set_rules('is_active', 'is_active', 'required');
-        //$this->form_validation->set_rules('jenis_hewan', 'jenis hewan', 'required');
-        //$this->form_validation->set_rules('jenis_kelamin', 'jenis kelamin', 'required');
-        //$this->form_validation->set_rules('umur', 'umur', 'required');
-        //$this->form_validation->set_rules('id_peternak', 'id_peternak', 'required');
-        //$this->form_validation->set_rules('daerah', 'daerah', 'required');
-        //$this->form_validation->set_rules('pekerjaan', 'pekerjaan', 'required');
-        //$this->form_validation->set_rules('alamat', 'alamat', 'required');
-        //$this->form_validation->set_rules('diagnosa', 'diagnosa', 'required');
-        //$this->form_validation->set_rules('vaksin', 'vaksin', 'required');
-       // $this->form_validation->set_rules('tahun', 'tahun', 'required');
-        
-        //$this->form_validation->set_rules('daerah', 'Daerah', 'required');
-        //$this->form_validation->set_rules('pekerjaan', 'Pekerjaan', 'required');
-        //$this->form_validation->set_rules('alamat', 'Alamat', 'required');
 
         if ($this->form_validation->run() == false) {
             $this->load->view('templates/header', $data);
@@ -83,10 +61,4 @@ class Usermanage extends CI_Controller
             redirect('usermanage');
         }
     }
-
-	//end of ubah 
-
-
-
-
 }
