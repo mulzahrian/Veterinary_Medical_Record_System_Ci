6fae8f2387626ef2698d4b59054feb1b572d9bda
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-10-27 09:14:33 +0700

    Update Medis.php
    little modify

--- application/controllers/Medis.php
+++ application/controllers/Medis.php
@@ -31,47 +31,26 @@ class Medis extends CI_Controller
         $this->form_validation->set_rules('vaksin', 'Vaksin', 'required');
         $this->form_validation->set_rules('tanggal', 'Tanggal', 'required');
         $this->form_validation->set_rules('tahun', 'Tahun', 'required');
-
-
         if($this->form_validation->run() == false ) {
-
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('medis/index', $data);
         $this->load->view('templates/footer');
-
-
     }else
      {
             $this->Medis_model->tambahDataMedis();
             $this->session->set_flashdata('flash', 'Ditambahkan');
             redirect('medis');
-        }
-
+     }
 function form_penjualan_autocomplit() {
         $id_hewan = $_GET['id_hewan'];
-        //$sql_barang = "SELECT tb.nama_barang,ts.satuan,tb.harga_jual,tb.stok_barang FROM tbl_barang as tb, tbl_satuan as ts where tb.barang_satuan=ts.id_satuan and kd_barang='$kd_barang' ";
         $sql_hewan = "SELECT * FROM `db_hewan` WHERE id_hewan='$id_hewan' ";
         $db_hewan = $this->db->query($sql_hewan)->row_array();
         $data = array(
             'id_hewan' => $db_hewan['id_hewan'],
             'jenis_hewan' => $db_hewan['jenis_hewan'],
-            
-            
         );
         echo json_encode($data);
     }
-
-
-  
-
-     
-    
-
-    
-
-    
-
-    
 }
 }
