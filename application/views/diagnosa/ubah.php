5e729c0868735008f7a53fea31268708655aa7f6
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-11-04 09:20:52 +0700

    Update ubah.php
    little modify

--- application/views/diagnosa/ubah.php
+++ application/views/diagnosa/ubah.php
@@ -11,37 +11,14 @@
                     <form action="" method="post">
                         <input type="hidden" name="id" value="<?= $diagnosa['id']; ?>">
 
-                        <!--div class="form-group">
-                            <label for="id_peternak">ID Peternak</label>
-                            <input type="text" name="id_peternak" class="form-control" id="id_peternak" value="<?= $id_peternak['id_peternak']; ?>">
-                            <!--small class="form-text text-danger"><?= form_error('jenis_hewan'); ?></small-->
-                        <!--/div-->
-                        
-
-                       
-                      
-                        
-
                         <div class="form-group">
                             <label for="diagnosa">Vaksin</label>
                             <input type="text" name="diagnosa" class="form-control" id="diagnosa" value="<?= $diagnosa['diagnosa']; ?>">
-                            <!--small class="form-text text-danger"><?= form_error('jenis_hewan'); ?></small-->
-                        </div>
-
-                       
-
-                        
-                       
-
-
-      
+                        </div>      
                         <button type="submit" name="ubah" class="btn btn-primary float-right">Ubah Data</button>
                     </form>
                 </div>
             </div>
-
-
         </div>
     </div>
-
 </div>
