c99b555558c70aafd200fb0daafea4991c0e6600
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-11-15 09:22:17 +0700

    Update index.php
    little modify

--- application/views/laporan/index.php
+++ application/views/laporan/index.php
@@ -1,8 +1,6 @@
 <div id="content-wrapper">
-
       <div class="container-fluid">
 
-        <!-- Breadcrumbs-->
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
             <a href="index.html"><?= $title; ?></a>
@@ -10,19 +8,12 @@
           <li class="breadcrumb-item active"><?= $title; ?></li>
         </ol>
 
-        <!-- Page Content -->
         <h1><?= $title; ?></h1>
         <hr>
         <div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
     <?php if ($this->session->flashdata('flash')) : ?>
 
-      <?php endif; ?>
-
-      <!--tambah data mahasiswa-->
-
-    <!-- end tambah data-->
-
-    <!-- form search -->
+    <?php endif; ?>
     <div class="row mt-3">
         <div class="col-md-6">
             <form action="" method="post">
@@ -31,26 +22,18 @@
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="submit">Cari</button>
                     </div>
-
             </form>
-
         </div>
         <hr>
-
   <div>
 
     <a href="<?= base_url(); ?>laporan/excel" class="btn btn-success"><i class="fa fa-clipboard"></i>  Excel File</a>
     </div>
     </div>
-
     </div>
 
 <hr>
-    <!-- end form search -->
-
-    <!-- isi data-->
     <div class="col-md">
-
     <table class="table table-hover">
   <thead>
     <tr>
@@ -73,12 +56,8 @@
       <td><?= $dr['pekerjaan'] ?></td>
       <td><?= $dr['id_hewan'] ?></td>
       <td><?= $dr['jenis_hewan'] ?></td> 
-      <td><?= $dr['diagnosa'] ?></td>
-      
+      <td><?= $dr['diagnosa'] ?></td>   
       <td>
-       
-
-      
       <a href="<?= base_url(); ?>laporan/hapus/<?= $dr['id']; ?>"
                         class="btn btn-danger btn-sm float-center tombol-hapus"><i class="fas fa-trash-alt"></i></a>
         <a href="<?= base_url(); ?>laporan/detail/<?= $dr['id']; ?>"
@@ -89,31 +68,10 @@
 
         <a href="<?= base_url(); ?>laporan/pdf/<?= $dr['id']; ?>"
         class="btn btn-info btn-sm float-center"><i class=" fas fa-print"></i></a>
-
-       
-
-        
-        
-
       </td>
     </tr>
     <?php $i++; ?>
     <?php endforeach ?>
-    
   </tbody>
 </table>
-
-
-    <!-- end isi data-->
-
-
-        <!-- card extensi foto dari boostrap dokumentasi -->
-
-
-
-
-        <!-- card end -->
-        
-
-
       </div>
