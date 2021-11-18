00f98dfe8933e886f9e097cb56ee2764befb5dcc
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-11-18 09:26:56 +0700

    Update ubah.php
    little modify

--- application/views/dokter/ubah.php
+++ application/views/dokter/ubah.php
@@ -1,8 +1,6 @@
 <div class="container">
-
     <div class="row mt-3">
         <div class="col-md-6">
-
             <div class="card">
                 <div class="card-header">
                     Form Ubah Data Hewan
@@ -11,46 +9,30 @@
                     <form action="" method="post">
                         <input type="hidden" name="id" value="<?= $id_peternak['id']; ?>">
 
-                        <!--div class="form-group">
-                            <label for="id_peternak">ID Peternak</label>
-                            <input type="text" name="id_peternak" class="form-control" id="id_peternak" value="<?= $id_peternak['id_peternak']; ?>">
-                            <!--small class="form-text text-danger"><?= form_error('jenis_hewan'); ?></small-->
-                        <!--/div-->
-
                         <div class="form-group">
                             <label for="nama">Nama</label>
                             <input type="text" name="nama" class="form-control" id="nama" value="<?= $id_peternak['nama']; ?>">
-                            <!--small class="form-text text-danger"><?= form_error('jenis_hewan'); ?></small-->
                         </div>
 
                         <div class="form-group">
                             <label for="daerah">Daerah</label>
                             <input type="text" name="daerah" class="form-control" id="daerah" value="<?= $id_peternak['daerah']; ?>">
-                            <!--small class="form-text text-danger"><?= form_error('jenis_hewan'); ?></small-->
                         </div>
 
                         <div class="form-group">
                             <label for="pekerjaan">Pekerjaan</label>
                             <input type="text" name="pekerjaan" class="form-control" id="pekerjaan" value="<?= $id_peternak['pekerjaan']; ?>">
-                            <!--small class="form-text text-danger"><?= form_error('jenis_hewan'); ?></small-->
                         </div>
 
                         <div class="form-group">
                             <label for="alamat">Alamat</label>
                             <input type="text" name="alamat" class="form-control" id="alamat" value="<?= $id_peternak['alamat']; ?>">
-                            <!--small class="form-text text-danger"><?= form_error('jenis_hewan'); ?></small-->
                         </div>
 
-
-
-      
                         <button type="submit" name="ubah" class="btn btn-primary float-right">Ubah Data</button>
                     </form>
                 </div>
             </div>
-
-
         </div>
     </div>
-
 </div>
